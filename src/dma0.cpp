8f1ad9e26ed4bbaa7f9669ad343c46ae54942211
josuemorais <josuemorais@gmail.com>
2025-02-11 07:27:24 -0300

    refactor dma0.cpp

--- src/dma0.cpp
+++ src/dma0.cpp
@@ -1,64 +1,82 @@
-#include <iikitmini.h>      // Biblioteca base do framework Arduino
+#include <iikitmini.h> // Biblioteca base do framework Arduino
 #include <driver/i2s.h>
 
-#define ADC_CHANNEL ADC1_CHANNEL_0  // GPIO36
-#define SAMPLE_RATE 1000
+#ifndef DMA_BUFFERS
 #define DMA_BUFFERS 4
+#endif
+
+#ifndef BUFFER_LEN
 #define BUFFER_LEN 64
-#define PLOT_INTERVAL 100  // Intervalo de plotagem em ms
+#endif
 
+typedef void (*ADCCallback)(const int16_t *data, size_t count);
+ADCCallback _adcDmaEspcallback;
 __attribute__((aligned(16))) int16_t dma_buffer[DMA_BUFFERS * BUFFER_LEN];
+uint32_t _plotInterval = 0;
+uint32_t _last_plot = 0;
 
-void setup() {
-  IIKit.setup();
-  
+void adcDmaSetup(adc1_channel_t channel, ADCCallback callback, adc_bits_width_t width_bit = ADC_WIDTH_BIT_12, uint32_t sample_rate = 1000, uint32_t plotInterval = 100)
+{
+  _adcDmaEspcallback = callback;
+  _plotInterval = plotInterval;
   adc_power_acquire();
-  adc1_config_width(ADC_WIDTH_BIT_12);
-  adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_12);
+  adc1_config_width(width_bit);
+  adc1_config_channel_atten(channel, ADC_ATTEN_DB_12);
 
   i2s_config_t i2s_config = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
-    .sample_rate = SAMPLE_RATE,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = DMA_BUFFERS,
-    .dma_buf_len = BUFFER_LEN,
-    .use_apll = false,
-    .tx_desc_auto_clear = false,
-    .fixed_mclk = 0,
-    .mclk_multiple = I2S_MCLK_MULTIPLE_256
-  };
+      .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
+      .sample_rate = sample_rate,
+      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+      .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+      .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+      .dma_buf_count = DMA_BUFFERS,
+      .dma_buf_len = BUFFER_LEN,
+      .use_apll = false,
+      .tx_desc_auto_clear = false,
+      .fixed_mclk = 0,
+      .mclk_multiple = I2S_MCLK_MULTIPLE_256};
 
-  if(i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL) != ESP_OK){
+  if (i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL) != ESP_OK)
+  {
     Serial.println("ERR:I2S_INIT");
-    while(1);
+    while (1)
+      ;
   }
 
-  i2s_set_adc_mode(ADC_UNIT_1, ADC_CHANNEL);
+  i2s_set_adc_mode(ADC_UNIT_1, channel);
   i2s_adc_enable(I2S_NUM_0);
 }
 
-void loop() {
-  IIKit.loop();    
-  static uint32_t last_plot = 0;
+void adcDmaLoop()
+{
   size_t bytes_read;
   esp_err_t err = i2s_read(I2S_NUM_0, dma_buffer, sizeof(dma_buffer), &bytes_read, 0);
-
-  if(err == ESP_OK) {
-    uint16_t samples = bytes_read / sizeof(int16_t);
-    
+  if (err == ESP_OK)
+  {
     // Envia todos os dados de uma vez para otimização
-    if(millis() - last_plot >= PLOT_INTERVAL) {
-      Serial.print(">adc:[");
-      for(int i = 0; i < samples; i++) {
-        uint16_t value = abs(dma_buffer[i]) & 0x0FFF;
-        Serial.print(value);
-        if(i < samples-1) Serial.print(",");
-      }
-      Serial.println("]|");
-      last_plot = millis();
+    if (millis() - _last_plot >= _plotInterval)
+    {
+      _adcDmaEspcallback(dma_buffer, (uint16_t) (bytes_read / sizeof(int16_t)));
+      _last_plot = millis();
     }
   }
+}
+
+// Função para leitura do ADC via DMA
+void readDMA(const int16_t *y, size_t ylen)
+{
+  IIKit.WSerial.plot("adcValue", (uint32_t) 1000, y, ylen); // Plota os valores do ADC no monitor serial
+}
+
+void setup()
+{
+  IIKit.setup();
+  adcDmaSetup(ADC1_CHANNEL_3, readDMA, ADC_WIDTH_BIT_12);
+}
+
+void loop()
+{
+  IIKit.loop();
+  adcDmaLoop();
 }
--- src/dma1.cpp
+++ src/dma1.cpp
@@ -1,35 +1,14 @@
 #include <iikitmini.h>      // Biblioteca base do framework Arduino
 #include "util/AdcDmaEsp.h" // Classe para configuração do ADC e DMA
-#include "util/jtask.h"     // Biblioteca para gerenciamento de tarefas com timers
 
-AdcDmaEsp adcDma; // Instância para gerenciar o ADC e DMA
-uint32_t timeCount = 0;
-
-// Função para leitura do ADC via DMA
-void readDMA(const uint16_t *buffer, size_t sampleCount)
-{
-    for (size_t i = 0; i < sampleCount; i++)
-    {
-        IIKit.WSerial.plot("adcValue",timeCount++, abs(buffer[i]) & 0x0FFF); // Plota os valores do ADC no monitor serial
-    }
-}
-
-// Configuração inicial
 void setup()
 {
-    IIKit.setup();
-    jtaskSetup();       
-    if(!adcDma.begin()) {
-        IIKit.WSerial.println("Failed to initialize ADC!");
-        while(1);
-    }
-    adcDma.setCallback(readDMA);                                // Configura o timer para 1000 Hz (1 ms)
-    jtaskAttachFunc([](){adcDma.update();}, 100);               // Processa os dados do ADC a cada 1 ms
+  IIKit.setup();
+  adcDmaSetup(ADC1_CHANNEL_3,[](const int16_t *y, size_t ylen){IIKit.WSerial.plot("adcValue", (uint32_t) 1000, y, ylen);}, ADC_WIDTH_BIT_12);
 }
 
-// Loop principal
 void loop()
 {
-    IIKit.loop();
-    jtaskLoop(); // Executa as tarefas enfileiradas
+  IIKit.loop();
+  adcDmaLoop();
 }
