5651585b604ca46d6c5f6d3e5fdb974098731798
Josué Silva de Morais <josuemorais@gmail.com>
2025-02-12 10:09:07 -0300

    refactor dma1

--- src/dma1.cpp
+++ src/dma1.cpp
@@ -3,7 +3,7 @@
 #include "util/jtask.h"
 
 uint32_t currentSample = 0;      // Índice da amostra atual
-#define RESOLUTION 256           // Número de pontos na senoide (quanto maior, mais suave)
+#define RESOLUTION 100           // Número de pontos na senoide (quanto maior, mais suave)
 #define MEIARESOLUTION 127.5
 
 void updateDAC() {
@@ -14,10 +14,10 @@ void updateDAC() {
 void setup()
 {
   IIKit.setup();
-  adcDmaSetup(CHANNEL_ADC1,[](const int16_t *y, size_t ylen){IIKit.WSerial.plot("adcValue", (uint32_t) 1000, y, ylen);}, ADC_WIDTH_BIT_12);
+  adcDmaSetup(ADC1_CHANNEL_0,[](const int16_t *y, size_t ylen){IIKit.WSerial.plot("adcValue", (uint32_t) 1000, y, ylen);}, ADC_WIDTH_BIT_12);
   
   // Calcula o tempo entre amostras com base na frequência inicial
-  unsigned long frequency = 10UL;                    // Frequência inicial da senoide em Hz  
+  unsigned long frequency = 100UL;                    // Frequência inicial da senoide em Hz  
   unsigned long sampleRate = frequency * RESOLUTION; // Número de amostras por segundo que queremos gerar.
   unsigned long samplePeriod = 1000000UL / sampleRate; // Tempo entre uma amostra e a próxima, em microssegundos (µs). OBS.: 1000000: Número de microssegundos em um segundo (1 segundo = 1.000.000 µs).
   jtaskAttachFunc(updateDAC, samplePeriod);  
